3cf28e83954896eb656907ba4fbd3f3d81572cf8
mcereal <mpbcurtis@gmail.com>
2021-01-27 17:54:29 -0500

    enable cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const session = require("express-session");
+const cors = require("cors");
 
 const passport = require("./config/passport");
 require("dotenv").config();
@@ -10,6 +11,7 @@ const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
