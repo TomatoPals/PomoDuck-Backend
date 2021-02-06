15e079a80ff13b049b885966004b7dd2b54cd9cc
mcereal <mpbcurtis@gmail.com>
2021-02-06 13:05:46 -0500

    merge conflicts

--- routes/api/user.js
+++ routes/api/user.js
@@ -3,12 +3,7 @@ const userController = require("../../controllers/userController");
 
 router.route("/createuser").post(userController.create);
 router.route("/getuser/:id").get(userController.findById);
-<<<<<<< HEAD
-router.route("/removeuser/:id").delete(userController.remove);
-router.route("/updateuser/:id").put(userController.update);
-=======
 router.route("/userremove/:id").delete(userController.remove);
 router.route("/userupdate/:id").put(userController.update);
->>>>>>> develop
 
 module.exports = router;
