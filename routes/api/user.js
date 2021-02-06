231051a1c1d81a0195b1042bca5c098090e269fd
mcereal <mpbcurtis@gmail.com>
2021-02-06 12:20:40 -0500

    updateing user routes to be uniform

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const userController = require("../../controllers/userController");
 
-router.route("/").post(userController.create);
-router.route("/:id").get(userController.findById);
-router.route("/remove/:id").delete(userController.remove);
-router.route("/update/:id").put(userController.update);
+router.route("/createuser").post(userController.create);
+router.route("/getuser/:id").get(userController.findById);
+router.route("/removeuser/:id").delete(userController.remove);
+router.route("/updateuser/:id").put(userController.update);
 
 module.exports = router;
