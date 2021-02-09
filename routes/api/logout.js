13c304f55034a0d9fa507f0d52bef2b6ab5918be
mcereal <mpbcurtis@gmail.com>
2021-02-09 14:24:41 -0500

    removed extraneous logout route

--- routes/api/logout.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const logoutController = require("../../controllers/logoutController");
-
-router.route("/").get(logoutController.logout);
-
-module.exports = router;
