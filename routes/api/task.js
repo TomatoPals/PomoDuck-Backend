18d7a7b833eb8852fedb5fa2aaeddf911af9f23d
Patrick <patrick.lloyd@theproducebox.com>
2021-02-11 19:44:03 -0500

    updating stuff

--- routes/api/task.js
+++ routes/api/task.js
@@ -5,5 +5,6 @@ router.route("/createtask").post(taskController.create);
 router.route("/gettask/:id").get(taskController.findById);
 router.route("/taskremove/:id").delete(taskController.remove);
 router.route("/taskupdate/:id").put(taskController.update);
+router.route("/findtasks/:id").get(taskController.findByUserId);
 
 module.exports = router;
